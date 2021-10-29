50b57a8ca46795d532ecf7dccc136ce8114346d0
MK Mozgawa <mzgw@protonmail.com>
2021-10-29 15:20:46 +0200

    Initial commit

---
+++ src/__init__.py
@@ -0,0 +1,16 @@
+from flask import Flask, jsonify
+from flask_restx import Resource, Api
+
+app = Flask(__name__)
+
+api = Api(app)
+app.config.from_object('src.config.DevelopmentConfig')
+
+class HelloWorld(Resource):
+    def get(self):
+        return {
+            'hello': 'world!'
+        }
+
+
+api.add_resource(HelloWorld, '/hello')
